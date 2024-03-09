05e563c2895674a3b85263e149f62c99c3108538
Berk Dwayne Onsoy <secure@elitepentest.com>
2024-03-09 16:23:49 +0100

    Projects

--- Login_Form/Login_Form.py
+++ Login_Form/Login_Form.py
@@ -1,42 +1,36 @@
 import tkinter
 from tkinter import messagebox
 
-
 window = tkinter.Tk()
 window.title("Login form")
-window.geometry('340x440')
-window.configure(bg='#333333')
+window.geometry("500x500")
+window.configure(bg="#1C1C1C")
+
+frame = tkinter.Frame(bg="#1C1C1C")
 
 def login():
-    username = "johnsmith"
-    password = "12345"
-    if username_entry.get()==username and password_entry.get()==password:
-        messagebox.showinfo(title="Login Success", message="You successfully logged in.")
+    username = "admin"
+    password = "Pa$$w0rd!"
+    if usernameEntry.get() == username and passwordEntry.get() == password:
+        messagebox.showinfo(title="Success!", message="Welcome back " + username + "!")
     else:
-        messagebox.showerror(title="Error", message="Invalid login.")
-
-frame = tkinter.Frame(bg='#333333')
+        messagebox.showinfo(title="Invalid Login!", message="Invalid username or password!")
 
 # Creating widgets
-login_label = tkinter.Label(
-    frame, text="Login", bg='#333333', fg="#FF3399", font=("Arial", 30))
-username_label = tkinter.Label(
-    frame, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-username_entry = tkinter.Entry(frame, font=("Arial", 16))
-password_entry = tkinter.Entry(frame, show="*", font=("Arial", 16))
-password_label = tkinter.Label(
-    frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-login_button = tkinter.Button(
-    frame, text="Login", bg="#FF3399", fg="#FFFFFF", font=("Arial", 16), command=login)
+loginLabel = tkinter.Label(frame, text="Login", bg="#1C1C1C", fg="#2F9DFF", font=("Monospace", 16, "bold"))
+usernameLabel = tkinter.Label(frame, text="Username", bg="#1C1C1C", fg="white", font=("Open Sans Light", 10))
+passwordLabel = tkinter.Label(frame, text="Password", bg="#1C1C1C", fg="white", font=("Open Sans Light", 10))
+usernameEntry = tkinter.Entry(frame, font=("Open Sans Light", 10))
+passwordEntry = tkinter.Entry(frame, show="*", font=("Open Sans Light", 10))
+loginButton = tkinter.Button(frame, text="Login", bg="#2F9DFF", fg="#FFFFFF", font=("Monospace", 10, "bold"), command=login)
 
 # Placing widgets on the screen
-login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
-username_label.grid(row=1, column=0)
-username_entry.grid(row=1, column=1, pady=20)
-password_label.grid(row=2, column=0)
-password_entry.grid(row=2, column=1, pady=20)
-login_button.grid(row=3, column=0, columnspan=2, pady=30)
-
 frame.pack()
+loginLabel.grid(row=0, column=1, columnspan=1, sticky="news", pady=20)
+usernameLabel.grid(row=1, column=0)
+usernameEntry.grid(row=1, column=1)
+passwordLabel.grid(row=2, column=0, pady=5)
+passwordEntry.grid(row=2, column=1, pady=5)
+loginButton.grid(row=3, column=1, columnspan=1, pady=10)
 
 window.mainloop()
